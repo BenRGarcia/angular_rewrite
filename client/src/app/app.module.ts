3f37041f9e8b9f760bf46e43b0e0647fe1b2141f
Ben Garcia <ben.garcia@globant.com>
2018-07-19 13:40:51 -0400

    add dashboard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GithubLoginComponent } from './github-login/github-login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubLoginComponent
+    GithubLoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
