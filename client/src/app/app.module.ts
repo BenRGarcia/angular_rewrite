ca325040d31d7af9b8449e35890b243bde1a3c3b
Ben Garcia <ben.garcia@globant.com>
2018-07-19 14:46:03 -0400

    extract routing specific logic

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { GithubLoginComponent } from './github-login/github-login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubLoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule // <- always leave as last indice, wildcard route found in module
   ],
   providers: [],
   bootstrap: [AppComponent]
