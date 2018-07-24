9aba640fc19f57e73f018c6bede0cdd41ef28850
Ben Garcia <ben.garcia@globant.com>
2018-07-24 11:05:09 -0400

    restart with new client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { MaterialModule } from './material';
-import { GithubLoginComponent } from './github-login/github-login.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GithubLoginComponent,
-    HomeComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    AppRoutingModule // <- always leave as last indice, wildcard route found in module
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
