a62f5de0dc39cd10ea709e8ac1bd724ef66f9107
Ben Garcia <ben.garcia@globant.com>
2018-07-24 11:52:55 -0400

    Add module with routing

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DashboardRoutingModule } from './dashboard-routing.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ],
   declarations: []
 })
