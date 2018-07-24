9759a102d0828413fdf689eb5062c40ef4605c73
Ben Garcia <ben.garcia@globant.com>
2018-07-24 11:30:43 -0400

    Add material module

---
+++ client/src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+} from '@angular/material';
+
+const MATERIAL_MODULES = [
+  MatButtonModule
+];
+
+@NgModule({
+  imports: MATERIAL_MODULES,
+  declarations: MATERIAL_MODULES
+})
+export class MaterialModule { }
