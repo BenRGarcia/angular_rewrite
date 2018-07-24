18700db7ce34932fc5d0f807e6d618f692e3319e
Ben Garcia <ben.garcia@globant.com>
2018-07-24 14:07:46 -0400

    Build out routing

--- client/src/app/core/containers/app.component.ts
+++ client/src/app/core/containers/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div>
+      wrapper of app.component.ts
       <router-outlet></router-outlet>
     </div>
   `
--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GetReposPageComponent } from './containers/get-repos-page.component';
+import { EditUserPageComponent } from './containers/edit-user-page.component';
+import { EditReposPageComponent } from './containers/edit-repos-page.component';
+import { TemplatesPageComponent } from './containers/templates-page.component';
+import { PreviewPageComponent } from './containers/preview-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'getrepos' },
-  { path: 'getrepos', component: GetReposPageComponent }
+  { path: 'getrepos', component: GetReposPageComponent },
+  { path: 'edituser', component: EditUserPageComponent },
+  { path: 'editrepos', component: EditReposPageComponent },
+  { path: 'template', component: TemplatesPageComponent },
+  { path: 'preview', component: PreviewPageComponent },
 ];
 
 @NgModule({
